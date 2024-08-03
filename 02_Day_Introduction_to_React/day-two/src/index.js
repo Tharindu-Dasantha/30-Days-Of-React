fb1d4e8781c19b08841734b3c9c4bfb5c218aa88
Dasantha Edirisinghe <t.dasanthaedirisinghe@gmail.com>
2024-08-03 21:09:28 +0530

    Completed the day 2

--- 02_Day_Introduction_to_React/day-two/src/index.js
+++ 02_Day_Introduction_to_React/day-two/src/index.js
@@ -1,47 +1,115 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 const headerStyles = {
-	fontFamily: 'Helvetica Neue',
-	padding: 25,
-	lineHeight: 1.5,
-}
+  backgroundColor: "#61dbfb",
+  color: "#ffffff",
+  textAlign: "center",
+  fontSize: 24,
+  fontWeight: "bold",
+  borderRadius: "10px",
+  fontFamily: "Helvetica Neue",
+  padding: 25,
+  lineHeight: 1.5,
+};
+
+const welcome = "Welcome to 30 Days of React";
+const title = "Getting Started React";
+const subtitle = "JavaScript Library";
+const author = {
+  firstName: "Tharindu",
+  lastName: "Edirisinghe",
+};
+const date = "Aug 1, 2024";
 
 const header = (
-	<header>
-	<h1>Welcome to 30 Days of React </h1>
-	<h2>Getting Started React</h2>
-	<h3>JavaScript Library</h3>
-	<p>Tharindu Edirisinghe</p>
-	<small>Aug 1, 2024</small>
-	</header>
-)
+  <header style={headerStyles}>
+    <h1>{welcome}</h1>
+    <h2>{title}</h2>
+    <h3>{subtitle}</h3>
+    <p>
+      Instructor: {author.firstName} {author.lastName}
+    </p>
+    <small>Date: {date}</small>
+  </header>
+);
+
+const numOne = 3;
+const numTwo = 2;
+
+const result = (
+  <p>
+    {numOne} + {numTwo} = {numOne + numTwo}
+  </p>
+);
+
+const yearBorn = 2002;
+const currentYear = new Date().getFullYear();
+const age = currentYear - yearBorn;
+const personAge = (
+  <p>
+    {""}
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+);
+
+const techs = ['HTML', 'CSS', "JavaScript"]
+const techsFormated = techs.map((tech) => <li>{tech}</li> )
+
+const mainStyles = {
+  backgroundColor: "#f3fof5",
+  padding: 20,
+  marginTop: 20,
+  fontFamily: "Helvetica Neue",
+  lineHeight: 1.5,
+  fontSize: 18,
+};
 
 const main = (
-	<main>
-	<p>Prerequisite to get started react.js</p>
-	<ul>
-	<li>HTML</li>
-	<li>CSS</li>
-	<li>JavaScript</li>
-	</ul>
-	</main>
-)
+  <main style={mainStyles}>
+    <div classname='main-wrapper'>
+		<p>
+			Prerequisites to get started{' '}
+			<strong>
+				<em>react.js</em>
+			</strong>
+			:
+		</p>
+		<ul>
+			{techsFormated}
+		</ul>
+		{result}
+		{personAge}
+	</div>
+  </main>
+);
+
+const copyRight = "Copyright 2020"
+
+const footerStyles = {
+  backgroundColor: "#61dbfb",
+  color: "#ffffff",
+  textAlign: "center",
+  fontSize: 16,
+  fontFamily: "Helvetica Neue",
+  padding: 20,
+  borderRadius: "10px",
+};
 
 const footer = (
-	<footer>
-	<p>Copyright 2024</p>
-	</footer>
-)
+  <footer style={footerStyles}>
+    <p>{copyRight}</p>
+  </footer>
+);
 
 const app = (
-	<div>
-	{header}
-	{main}
-	{footer}
-	</div>
-)
+  <div classname="app">
+    {header}
+    {main}
+    {footer}
+  </div>
+);
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(app, rootElement)
+ReactDOM.render(app, rootElement);
